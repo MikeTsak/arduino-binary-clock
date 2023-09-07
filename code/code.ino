325576bfd029aa5cc477394c1fdd2399c0c3508c
Mike Tsakiroglou <32255755+MikeTsak@users.noreply.github.com>
2023-09-07 18:42:20 +0300

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,17 +1,68 @@
-// Define the LED pin
-const int ledPin = 13;
+// Define pins
+const int hourPins[] = {2, 3, 4, 5};      // pins for hours in binary
+const int minutePins[] = {6, 7, 8, 9, 10, 11};  // pins for minutes in binary
+const int secondPins[] = {A0, A1, A2, A3, A4, A5};  // pins for seconds in binary
+
+const int onboardLED = 13;  // onboard LED pin
+
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
 
 void setup() {
-  // Initialize the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  // Initialize pins as outputs
+  for (int pin : hourPins) {
+    pinMode(pin, OUTPUT);
+  }
+  for (int pin : minutePins) {
+    pinMode(pin, OUTPUT);
+  }
+  for (int pin : secondPins) {
+    pinMode(pin, OUTPUT);
+  }
+
+  pinMode(onboardLED, OUTPUT); // set onboard LED as output
+
+  // Start an interrupt for counting seconds
+  cli();  // stop interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  OCR1A = 15624;  // compare match register, 1 Hz
+  TCCR1B |= (1 << WGM12);  // CTC mode
+  TCCR1B |= (1 << CS10) | (1 << CS12);  // 1024 prescaler
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  sei();  // allow interrupts
 }
 
 void loop() {
-  // Turn the LED on
-  digitalWrite(ledPin, HIGH);
-  delay(1000); // Wait for 1 second
+  // Main loop is empty, as everything is handled by the interrupt
+}
 
-  // Turn the LED off
-  digitalWrite(ledPin, LOW);
-  delay(2000); // Wait for 1 second
+void displayBinary(int number, const int pins[], int numPins) {
+  for (int i = 0; i < numPins; i++) {
+    digitalWrite(pins[i], (number >> i) & 1);
+  }
+}
+
+ISR(TIMER1_COMPA_vect) {
+  seconds++;
+  digitalWrite(onboardLED, !digitalRead(onboardLED)); // Toggle onboard LED state
+
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+      if (hours >= 12) {
+        hours = 0;
+      }
+    }
+  }
+
+  displayBinary(hours, hourPins, sizeof(hourPins) / sizeof(int));
+  displayBinary(minutes, minutePins, sizeof(minutePins) / sizeof(int));
+  displayBinary(seconds, secondPins, sizeof(secondPins) / sizeof(int));
 }
